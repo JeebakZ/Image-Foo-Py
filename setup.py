fea0839a52074b9c0d5358a3543245dc94da360c
Jeebak <137192384+JeebakZ@users.noreply.github.com>
2024-08-02 16:30:56 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='your_project_name',
+    version='0.1',
+    install_requires=[
+        'opencv-python',
+        'numpy',
+        'streamlit',
+    ],
+)
